00e9485fc537b9d08c26afa856e99ae13d11805c
sanan244 <sanchezanthony244@gmail.com>
2021-09-26 18:41:47 -0700

    anthony's update

--- consumer.cpp
+++ consumer.cpp
@@ -14,14 +14,12 @@ const int SIZE = 4096;
 const char *name = "OS";
 
 int shm_fd;
-
 void *ptr;
 
 shm_fd = shm_open(name,O_RDONLY, 0666);
-
 ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
 
-printf("%s", (char *)ptr);
+printf("%s", "%s", "%s", "%s", "%s", "%s", "%s", (char *)ptr);
 
 shm_unlink(name);
 return 0;
--- producer.cpp
+++ producer.cpp
@@ -17,15 +17,22 @@ int TAT;
 
 int main(int id1[], int at1[], int bt1[], int id2[], int at2[], int bt2){
 
+// configure share memory 
 const int SIZE = 4096;
-
 const char *name = "OS";
 
-const char *message_0 = "Hello";
-const char *message_1 = "World";
+// create variables for command line input
+int *id1_ = id1;
+int *id2_ = id2;
+
+int *at1_ = at1;
+int *at2_ = at2;
+
+int *bt1_ = bt1;
+int *bt2_ = bt2;
+
 
 int shm_fd;
-
 void *ptr;
 
 shm_fd = shm_open(name,O_CREAT | O_RDWR, 0666);
@@ -34,10 +41,18 @@ ftruncate(shm_fd, SIZE);
 
 ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
+// write to the shared memory object
 sprintf((char*)ptr, "%s", message_0);
 *ptr += strlen(message_0);
 
 sprintf((char *)ptr, "%s", message_1);
 *ptr += strlen(message_1);
+
+sprintf((char*)ptr, "%s", id1_);
+sprintf((char *)ptr, "%s", id2_);
+sprintf((char *)ptr, "%s", at1_);
+sprintf((char *)ptr, "%s", at2_);
+sprintf((char *)ptr, "%s", bt1_);
+sprintf((char *)ptr, "%s", bt2_);
 
 }
