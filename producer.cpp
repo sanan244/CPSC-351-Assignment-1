8c3afc4568be9cbaf1ee35e9f0f5c606615d5050
sanan244 <sanchezanthony244@gmail.com>
2021-09-26 17:16:16 -0700

    producer file

---
+++ producer.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+
+struct process{
+int ID;
+int AT;
+int BT;
+int interupt_cnt;
+int TAT;
+}Q0[10], Q1[10], Q2[10];// Three Queues
+
+int main(){
+
+const int SIZE = 4096;
+
+const char *name = "OS";
+
+const char *message_0 = "Hello";
+const char *message_1 = "World";
+
+int shm_fd;
+
+void *ptr;
+
+shm_fd = shm.open(name,0_CREAT | 0_RDWR, 0666);
+
+ftruncate(shm_fd, SIZE);
+
+ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+sprintf(ptr, "%s", message_0);
+ptr += strlen(message_0);
+
+sprintf(ptr, "%s", message_1);
+ptr += strlen(message_1);
+
+}
