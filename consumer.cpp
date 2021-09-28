5a0abee4edaa01aa8875e9d6ae135430fb264d69
Kevin <kaoghk@gmail.com>
2021-09-28 12:26:08 -0700

    test

--- consumer.cpp
+++ consumer.cpp
@@ -15,11 +15,11 @@ const char *name = "OS";
 
 int shm_fd;
 
-void *ptr;
+char *ptr;
 
 shm_fd = shm_open(name,O_RDONLY, 0666);
 
-ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+ptr = (char*)mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
 
 printf("%s", (char *)ptr);
 
--- producer.cpp
+++ producer.cpp
@@ -15,7 +15,7 @@ int interupt_cnt;
 int TAT;
 }Q0[10], Q1[10], Q2[10];// Three Queues
 
-int main(int id1[], int at1[], int bt1[], int id2[], int at2[], int bt2){
+int main(){
 
 const int SIZE = 4096;
 
@@ -26,18 +26,18 @@ const char *message_1 = "World";
 
 int shm_fd;
 
-void *ptr;
+char *ptr;
 
 shm_fd = shm_open(name,O_CREAT | O_RDWR, 0666);
 
 ftruncate(shm_fd, SIZE);
 
-ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+ptr = (char*)mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
-sprintf((char*)ptr, "%s", message_0);
-*ptr += strlen(message_0);
+sprintf(ptr, "%s", message_0);
+ptr += strlen(message_0);
 
-sprintf((char *)ptr, "%s", message_1);
-*ptr += strlen(message_1);
+sprintf(ptr, "%s", message_1);
+ptr += strlen(message_1);
 
 }
