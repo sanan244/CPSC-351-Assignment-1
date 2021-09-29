d7c1eb80f617e318cb986d64de93510922eb5a18
jackloague1 <55412612+jackloague1@users.noreply.github.com>
2021-09-28 20:58:21 -0700

    Update for command line input

--- producer.cpp
+++ producer.cpp
@@ -9,68 +9,62 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
+struct process{
+int ID;
+int AT;
+int BT;
+int interupt_cnt;
+int TAT;
+}Q0[10], Q1[10], Q2[10];// Three Queues
 
+int main(int argc, char *argv[]){
 
-int main(){
-// configure share memory
-const int SIZE = 4096;
-const char *name = "OS";
-char shm_fd;
-void *ptr;
+int i = 1;
+int j = 0;
+int arraySize = argc/3; // Divide number of command line arguments by 3
+process processArray[arraySize]; // Set this number to the size of the array
 
-// set input variables
-int num;
- char id1_[3];
- char at1_[3];
- char bt1_[3];
-
- //ask for process count
- printf("How many processes would you like to add?:");
- cin >> num;
- char *previous_ptr;
- // Loop will create a vector of processes
- for(int i = 1; i < num + 1; i++){
-
-   cout << "Enter ID for process " << i << ":";
-   cin >> id1_;
-   cin.clear();
-   cout << "Enter AT for process " << i << ":";
-   cin >> at1_;
-   cin.clear();
-   cout << "Enter BT for process " << i <<":";
-   cin >> bt1_;
-   cin.clear();
-/*
-   // add input to struct then add struct to back of vector
-   cur_process.ID = id1_;
-   cur_process.AT = at1_;
-   cur_process.BT = bt1_;
-   process_.push_back(cur_process);
-*/
-//printf("...Input ready.\n");
-
-shm_fd = shm_open(name,O_CREAT | O_RDWR, 0666);
-//printf("...shm opened.\n");
-ftruncate(shm_fd, SIZE);
-//printf("...ftruncated.\n");
-ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
-printf("...ptr mapped.\n");
-
-// write to the shared memory object
-//ptr += sprintf((char *)ptr, "%s ", previous_ptr);
-//ptr += strlen(id1_);
-ptr += sprintf((char *)ptr, "%s ", id1_);
-//ptr += strlen(id1_);
-ptr += sprintf((char *)ptr, "%s ", at1_);
-//ptr += strlen(at1_);
-ptr += sprintf((char *)ptr, "%s", bt1_);
-//ptr += strlen(bt1_);
-//previous_ptr = id1_ + at1_ + bt1_;
-//cout << "...Previous ptr:" << previous_ptr << "\n";
-printf("...Process written to memory.\n");
+// Assign ID, AT, and BT to each processArray element
+while(j < arraySize)
+{
+    processArray[j].ID = atoi(argv[i]);
+    i++;
+    processArray[j].AT = atoi(argv[i]);
+    i++;
+    processArray[j].BT = atoi(argv[i]);
+    i++;
+    j++;
 }
 
-printf("Producer complete.\n");
-return 0;
+// Check for correct data by printing the elements of the array
+for(int i=0; i < arraySize; i++)
+{
+    std::cout << processArray[i].ID << " ";
+    std::cout << processArray[i].AT << " ";
+    std::cout << processArray[i].BT << " ";
+}
+
+const int SIZE = 4096;
+
+const char *name = "OS";
+
+const char *message_0 = "Hello";
+const char *message_1 = "World";
+
+int shm_fd;
+
+void *ptr;
+
+shm_fd = shm_open(name,O_CREAT | O_RDWR, 0666);
+
+ftruncate(shm_fd, SIZE);
+
+ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+sprintf((char*)ptr, "%s", message_0);
+ptr += strlen(message_0);
+
+sprintf((char*)ptr, "%s", message_1);
+ptr += strlen(message_1);
+
 }
