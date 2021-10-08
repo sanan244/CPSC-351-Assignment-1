72d013778cffe6970cef1097c05706f6e9e0a074
jackloague1 <55412612+jackloague1@users.noreply.github.com>
2021-10-07 22:55:04 -0700

    Update consumer.cpp

--- consumer.cpp
+++ consumer.cpp
@@ -5,85 +5,107 @@
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
-#include <list>
-#include <algorithm>
-#include <vector>
-#include <string>
+#include <unistd.h>
+#include <queue>
+#include <sys/mman.h>
 #include <iostream>
 
-//Global struct and vector
-// Struct for each process
 struct process{
-char* ID;
-char* AT;
-char* BT;
-char charerupt_cnt;
-char TAT;
-}Q0[10], Q1[10], Q2[10];// Three Queues
-// Vector of all dynamically created processes
-std::vector<char *>ptr_input;
+std::string id{""};
+std::string at{""};
+std::string bt{""};
+int qnum{0};
+int exit_time{1};
+int num_of_interrupts{0};
+};
 
-// add input variables into digits list
-void split(std::list<int>&digits, int x){
-  if (0 == x) {
-      digits.push_back(0);
-    }
-  else {
-      while (x != 0) {
-        int last = x % 10;
-        digits.push_front(last);
-        x = (x - last) / 10;
-      }
+// Fills the process array with the data from producer.cpp
+void fillProcessArray(std::string ptrString, process processArray[], int processArraySize, int i)
+{
+    int j;
+
+    for(j=0; j < processArraySize; j++)
+    {
+        while(isdigit(ptrString[i]) || isalpha(ptrString[i]))
+        {
+            processArray[j].id += ptrString[i];
+            i++;
+        }
+        i++;
+        while(isdigit(ptrString[i]) || isalpha(ptrString[i]))
+        {
+            processArray[j].at += ptrString[i];
+            i++;
+        }
+        i++;
+        while(isdigit(ptrString[i]) || isalpha(ptrString[i]))
+        {
+            processArray[j].bt += ptrString[i];
+            i++;
+        }
+        i++;
     }
 }
 
-int main(){
+int main()
+{
+// intilaizing shared memory space
 
-//Initialize variables
+/* the size (in bytes) of shared memory object */
 const int SIZE = 4096;
-const char *name = "OS";
-int shm_fd;
-void *ptr;
-bool exit = false;
+/* name of the shared memory object */
+const char *name = "Shared Memory";
+/* shared memory file descriptor */
+int fd;
+/* pointer to shared memory obect */
+char *ptr;
+/* open the shared memory object */
+fd = shm_open(name, O_RDWR, 0666);
+/* memory map the shared memory object */
+ptr = (char *) mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+/* read from the shared memory object */
 
-// loop will continue to read shared memory until the ptr value is 'done'
-// when ptr = 'done' exit = TRUE
-//while(exit == false){
-    // read shared memory
-    shm_fd = shm_open(name,O_RDONLY, 0666);
-    ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+std::vector<process> process_vec;
 
-    //only add new ptr if it does not exist
-  //  if(find(ptr_input.begin(), ptr_input.end(),(char *)ptr)  == ptr_input.end()){
-        printf("%s", (char *)ptr);
-        std::cout << "\n";
-        ptr_input.push_back((char *)ptr);
+std::cout << "passed c string: ";
+printf("%s",(char *)ptr);
+std::cout << std::endl;
 
-        // turn string input into int
-        int x;
-        //sscanf((char *)ptr, "%d", &x);
-        //printf("\nThe value of int x is: %d\n", x);
+std::string ptrString = std::string(ptr); // A variable that will hold the contents of "ptr" in a string format
 
-        //split int variable into digits and add all digits to the list 'digits' above
-        //split(digits,x);
-        shm_unlink(name);
-  //}
+std::string stringProcessArraySize; // The size of the array in a string format
+int processArraySize; // The size of the array
 
+int i = 0;
 
-//}//end of exit Loop
-
-/*
-//Travers digits list and execute scheduling algorithm according to each variable
-for (it = digits.begin(); it != digits.end(); ++it){
-  if(it == 0){
-    id1 = digits[it];
-    printf(id);}
-  else if(it == 1){}
-  else if(it == 2){}
-  else if(it == 3){}
-  else if(it == 4){}
-  else{bt2 = digits[it]}
+// Retrieves the number of processes and stores it in a string format
+while(isdigit(ptrString[i]))
+{
+    stringProcessArraySize += ptrString[i];
+    i++;
 }
-*/
+i++;
+
+// Converts the number of processes from a string to an int
+processArraySize = std::stoi(stringProcessArraySize);
+
+// Check to see if the number of processes is correct
+std::cout << "Number of processes: " << processArraySize << std::endl;
+
+process processArray[processArraySize]; // Creates an array of processes with the number of processes as its size
+
+// Parses the data from ptrString to a process array
+fillProcessArray(ptrString, processArray, processArraySize, i);
+
+// Check to see if the contents of the array are correct by printing the data
+std::cout << "Processes Data: " << std::endl;
+for(i=0; i< processArraySize; i++)
+{
+    std::cout << "Process Id: " << processArray[i].id << " ";
+    std::cout << "Arrival Time: " << processArray[i].at << " ";
+    std::cout << "Burst Time: " << processArray[i].bt;
+    std::cout << std::endl;
+}
+
 return 0;
 }
